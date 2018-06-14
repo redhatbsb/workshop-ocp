1a08f7a4df0c7b192bd5729a313cd78cc814001f
redhatbsb <40247249+redhatbsb@users.noreply.github.com>
2018-06-14 11:38:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
-echo "<h1>Openshift Workshop v2.3</h1> ";
+echo "<h1>Openshift Workshop v2.4</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
-$conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
+$userdb = getenv("dbuser");
+$passdb = getenv("dbpass");
+
+$conn = new mysqli("mysql", $userdb, $passdb, "workshop");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
